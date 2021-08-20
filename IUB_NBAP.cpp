be69cfcbec6447afbbf41d5e19953d02025ab119
copslock <34976676+copslock@users.noreply.github.com>
2021-08-20 22:23:57 +0800

    Create IUB_NBAP.cpp

---
+++ IUB_NBAP.cpp
@@ -0,0 +1,12 @@
+NBAP of NodeB protocol stack
+#TODO
+Basic procedure messages of Setup NodeB & PS domain call.
+Node-B Initialization
+UE                Node-B             RNC MSC
+Audit Procedure
+Cell Setup Procedure
+System Information Update
+Common TrCH(PRACH, AICH, RACH) Setup
+Common TrCH(SCCPCH, PCH/PICH, FACHs) Setup
+SIB5 System Information Update
+Un-block and Un-barred Cell
